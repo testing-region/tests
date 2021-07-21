554f6947f0df2a2f803a65f955e9b974f99c96c8
David Saah <dasorange.hope@gmail.com>
2021-07-21 16:46:17 +0000

    Draw rectangle with turtle

---
+++ python/rect.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+width(4)
+
+forward(200)
+right(90)
+
+pencolor('red')
+forward(100)
+right(90)
+
+pencolor('green')
+forward(200)
+right(90)
+
+pencolor('blue')
+forward(100)
+right(90)
+
+done()
