542e961009cff253eebd2db28de2878f9c7efd53
David Saah <dasorange.hope@gmail.com>
2021-08-01 15:46:16 +0000

    Updated auth.py

--- python/Flask/Sessions/app/auth.py
+++ python/Flask/Sessions/app/auth.py
@@ -10,7 +10,6 @@ from app.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-
 @bp.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -39,7 +38,6 @@ def register():
     
     return render_template('auth/register.html')
     
-
 @bp.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -65,7 +63,6 @@ def login():
     
     return render_template('auth/login.html')
 
-
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
@@ -77,5 +74,16 @@ def load_logged_in_user():
             'SELECT * FROM user WHERE id = ?', (user_id, )
         ).fetchone()
 
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
 
