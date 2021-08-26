762027b0816a690723f54017a53b50e7c3523403
David Saah <dasorange.hope@gmail.com>
2021-08-26 22:59:10 +0000

    Modify friend function

--- c-plus-plus/friend/friend.cpp
+++ c-plus-plus/friend/friend.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 
 class Stank{
-	public:
-		Stank(){
-			stinky = 0;
-		}
-	private:
-		int stinky;
-	
-	friend void stanksFriend(Stank &sfo);
+    public:
+        Stank(){
+	    stinky = 0;
+	}
+    private:
+	int stinky;
+
+        friend void stanksFriend(Stank &sfo);
 };
 
 void stanksFriend(Stank &sfo){
-	sfo.stinky = 99;
-	cout << sfo.stinky << endl;
+    sfo.stinky = 99;
+    cout << sfo.stinky << endl;
 }
 
 
 int main(){
-	Stank bob;
-	stanksFriend(bob);
+    Stank bob;
+    stanksFriend(bob);
 }
