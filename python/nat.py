b9abe64d7d6b9bf2c38effad2636410ae3831482
David Saah <dasorange.hope@gmail.com>
2021-10-02 18:23:55 +0000

    add billing checker with time

--- python/nat.py
+++ python/nat.py
@@ -1,12 +1,16 @@
-nums = []
 
-for x in range(20):
-	a = float(input("Enter a number: "))
-	nums.append(a)
+def bill(start_hour, start_min, end_hour, end_min):
+    normal_hours = 21 - start_hour
+    late_hours = end_hour - 21
+    if end_hour <= 21:
+        return (normal_hours + (start_min + end_min)/60) * 30
+    elif end_hour == 21 and end_min > 0:
+        return (normal_hours + start_min/60) * 30 + (end_min/60 * 25)
+    elif end_hour > 21:
+        return (normal_hours + start_min/60) * 30 + ((late_hours + end_min/60) * 25)
+    else:
+        return 0
 
-sum = 0
-
-for num in nums:
-	sum += num
-
-print(sum)
+# Test
+print(bill(9, 0, 21, 0))
+print(bill(9, 0, 21, 30))
