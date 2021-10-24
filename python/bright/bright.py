8fa1bdd4e45653d664a1c1a4214e90a6b8b3e950
David Saah <dasorange.hope@gmail.com>
2021-10-24 12:24:36 +0000

    delete bright files

--- python/bright/bright.py
+++
@@ -1,9 +0,0 @@
-def divisors(n):
-    sum = 0
-    for x in range(1, n):
-        if n % x == 0:
-            sum += x
-    return sum
-
-num = int(input('Enter the number: '))
-print(divisors(num))
