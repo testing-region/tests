4df27a3ef5ddcb6f2be6e83116c57d04a1eff8b6
David Saah <dasorange.hope@gmail.com>
2021-11-06 21:00:59 +0000

    add cpp optimized I/O with files

---
+++ c-plus-plus/cp-algorithms/io2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // to get a whole line
+    string s;
+    getline(cin, s);
+
+    cout << s << "\n";
+
+
+    // reads element from the input, one after another, until there is no more data available in the input
+    while (cin > x) {
+    	// code
+    }
+
+
+    // In some contest systems, files are used for input and output. An easy solution
+    // for this is to write the code as usual using standard streams, but add the following
+    // lines to the beginning of the code:
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    // the program reads the input from the file ”input.txt” and writes the
+    // output to the file ”output.txt”.
+}
