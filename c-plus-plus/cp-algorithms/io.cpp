74973f91207276f7c20e87a54b047f183c77af5e
David Saah <dasorange.hope@gmail.com>
2021-11-06 21:00:22 +0000

    add simple cpp optimized I/O

---
+++ c-plus-plus/cp-algorithms/io.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    // To make i/o more efficient add the lines below at the beginning of the code
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    string x;
+
+    cin >> a >> b >> x;
+    cout << a << " " << b << " " << x << "\n";
+    // "\n" works faster than endl
+}
+
