a35816ee5589a0d9a62dbd249cf9af4fdf852365
David Saah <dasorange.hope@gmail.com>
2021-11-06 20:55:16 +0000

    test cpp optimized compilation

--- c-plus-plus/test/test.cpp
+++ c-plus-plus/test/test.cpp
@@ -1,12 +1,7 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-
-int main(){
-	srand(time(0));    //allows to alter the rand algorithm to make the numbers truly random
-	for(int x = 1; x < 25; x++){ 
-		cout << 1 + (rand() % 6) << endl;
-	}
+int main() {
+	cout << "Hello world";
 }
