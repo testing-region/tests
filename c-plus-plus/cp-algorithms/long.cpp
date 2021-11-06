72010851d10736557eb37279f5b507b469f95bc2
David Saah <dasorange.hope@gmail.com>
2021-11-06 20:59:31 +0000

    add long data type test

---
+++ c-plus-plus/cp-algorithms/long.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // typedef is used to give a shorter name to a datatype
+    // Sample:
+    typedef long long ll;
+
+    ll a = 123456789;
+    ll b = 987654321;
+    cout << a*b << '\n';
+}
