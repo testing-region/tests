874d80c812a593f5a37b8576a937ef01bad1f7e8
David Saah <dasorange.hope@gmail.com>
2022-04-20 11:29:45 +0000

    add work3 part a

---
+++ python/class work/work3/work3a.py
@@ -0,0 +1,10 @@
+"""
+Write a simple Python program that displays the following powers
+of 2, one per line:   
+    2^1, 2^2, 2^3, 2^4, 2^5, 2^6, 2^7, 2^8
+"""
+
+# use a loop to get a range of numbers from 1 to 8
+# raise 2 to that power and print on the screen
+for x in range(1, 9):
+    print(2 ** x)
