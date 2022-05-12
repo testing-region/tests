15e36adb621d9f00ea8b048350412505402e86b5
David Saah <dasorange.hope@gmail.com>
2022-05-12 09:24:25 +0000

    add work 5

--- python/class work/work 5/Q2.py
+++ python/class work/work 5/Q2.py
@@ -1,21 +1,6 @@
-"""
-Rewrite your pay computation with time-and-a-half for overtime and
-create a function called computepay which takes two parameters (hours and  rate).
-Enter Hours: 45
-Enter Rate: 10
-Pay: 475.0
-Note that 475 = 40 * 10 + 5 * 15
-"""
-
-
 # Display message about program
 print("Welcome to Gross Pay Checker")
-print("[!] Enter the number of hours worked and the rate charged(in cedis) per hour")
 print("-"*30)
-
-# Get input from user
-hours = float(input("Enter the hours worked: "))
-rate = float(input("Enter the hourly rate: "))
 
 def computepay(hours, rate):
     """compute the pay for the given hours and rate"""
@@ -27,9 +12,10 @@ def computepay(hours, rate):
         gross_pay = (40 * rate) + (1.5 * rate * extra_hours)
     return gross_pay
 
+hours = 45
+rate = 10
 gross_pay = computepay(hours, rate)
 
 # Output message
-print("\n")
 print(f"After working {hours} hours, your gross pay is {gross_pay} cedis")
 print("We value your service")
