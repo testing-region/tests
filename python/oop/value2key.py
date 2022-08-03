d4df17563cfa87b2a8d55c68ac2d80d92af62a78
David Saah <dasorange.hope@gmail.com>
2022-08-03 00:20:35 +0000

    test how to find key from a value in dict

---
+++ python/oop/value2key.py
@@ -0,0 +1,24 @@
+# Goal: Find the key using the value in a dictionary
+
+from re import S
+
+
+sample_dict = {
+    "love": 23,
+    "peace": 42,
+    "joy": 17,
+}
+
+# val = 23
+
+# keys = sample_dict.keys()
+# vals = sample_dict.values()
+# items = sample_dict.items()
+# print(keys)
+# print(vals)
+# print(items)
+
+print(max(sample_dict))
+
+# for x, y in items:
+#     print(x, y)
