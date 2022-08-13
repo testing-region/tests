8c4be71873f4985ac0efbb3e9939cecad46302d8
David Saah <dasorange.hope@gmail.com>
2022-08-13 12:27:05 +0000

    rm unnecessary comments

--- tdd/fizzbuzz.py
+++ tdd/fizzbuzz.py
@@ -1,15 +1,6 @@
 class FizzBuzz:
 
     def fizzbuzz(self, num):
-        # if num == 3:
-        #     return "Fizz"
-        # elif num == 5:
-        #     return "Buzz"
-        # return num
-
-        # A great design should most likely have one exit point for a function
-        # A good suggestion would be to have the return value stored in a variable and have just one exit point
-        
         msg = num
 
         if num == 3:
