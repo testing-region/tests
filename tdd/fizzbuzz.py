504238770f3ed1eb9beb16b3f92d697772a3a2a2
David Saah <dasorange.hope@gmail.com>
2022-08-13 12:32:35 +0000

    add fizzbuzz

--- tdd/fizzbuzz.py
+++ tdd/fizzbuzz.py
@@ -3,9 +3,11 @@ class FizzBuzz:
     def fizzbuzz(self, num):
         msg = num
 
-        if num == 3:
+        if num % 3 == 0 and num % 5 == 0:
+            msg = "FizzBuzz"
+        elif num % 3 == 0:
             msg = "Fizz"
-        elif num == 5:
+        elif num % 5 == 0:
             msg = "Buzz"
 
         return msg
