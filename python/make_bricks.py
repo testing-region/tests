1cbb7aff69182c3c0a7706c6a9cdde1a3af8aa43
David Saah <dasorange.hope@gmail.com>
2022-08-16 10:31:46 +0000

    add make bricks

---
+++ python/make_bricks.py
@@ -0,0 +1,10 @@
+def make_bricks(small, big, goal):
+  if goal > (big * 5 + small):
+    return False
+  elif small < (goal % 5):
+    return False
+  elif goal // 5 < big:
+      return False
+  else:
+    return True
+
