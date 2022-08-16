afd082ef543078cd66a2d841da05945245849c7b
David Saah <dasorange.hope@gmail.com>
2022-08-16 11:48:20 +0000

    update subtract product from sum to use modulos

--- python/addprod.py
+++ python/addprod.py
@@ -2,5 +2,13 @@ import math
 
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
-        val = [int(x) for x in str(n)]
-        return math.prod(val) - sum(val)
+        sum_ = 0
+        mul_ = 1
+
+        while n > 0:
+            temp = n % 10
+            mul_ *= temp
+            sum_ += temp
+            n //= 10
+
+        return mul_ - sum_
