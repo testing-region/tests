25191eb6728499127932f9553a96a583f32d5502
David Saah <dasorange.hope@gmail.com>
2022-08-31 17:32:24 +0000

    add lab1

---
+++ python/CS212/lab1/lab1.py
@@ -0,0 +1,10 @@
+import math
+
+
+def display_cone_area(radius, slant_length):
+    area = (math.pi * radius**2) + (math.pi * radius * slant_length)
+    return round(area, 2)
+
+def display_cone_volume(radius, height):
+    volume = (1/3) * math.pi * radius**2 * height
+    return round(volume, 2)
