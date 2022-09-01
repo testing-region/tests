d94745f21df6f3e7cfc386140c0b2a051debe714
David Saah <dasorange.hope@gmail.com>
2022-09-01 21:25:27 +0000

    test named arguments dart

--- dart/main.dart
+++ dart/main.dart
@@ -1,7 +1,12 @@
-int addNumbers(int x, int y) {
+// testing named arguments
+// put parameters into curly braces
+// all named arguments are optional, hence your code should 
+// implement a logic that would run if no arguments are passed
+// else, you'll get an error
+int addNumbers({int x = 0, int y = 0}) {
     return x + y;
 }
 
 void main() {
-    print(addNumbers(20, 5));
+    print(addNumbers(x: 20, y:5));
 }
