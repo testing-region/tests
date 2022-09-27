c7d7218cee327d3a299451d75cbca9625fa301cf
David Saah <dasorange.hope@gmail.com>
2022-09-27 11:10:29 +0000

    add comments to binary search & refactor exit base case

--- DSA/binary-search/test1.py
+++ DSA/binary-search/test1.py
@@ -8,11 +8,20 @@ stop = len(arr) - 1
 
 
 def binary_search(arr, target, start, stop):
+    
+    # get the mid index of the start and stop indices in the 
+    # array to avoid slicing the array.
+
+    # no slicing prevents wrong indices from being returned if 
+    # the target number is present in the array.
     mid_index = math.floor((start+stop)/2)
     
     if arr[mid_index] == target:
         return mid_index
-    elif len(arr[start:stop]) == 0:
+    
+    # if the end of the binary search is reached, the start and 
+    # stop values will be the same, hence stop the recursive call.
+    if start == stop:
         return "Number not present in array"
     
     if arr[mid_index] > target:
@@ -21,4 +30,6 @@ def binary_search(arr, target, start, stop):
         return binary_search(arr, target, mid_index + 1, stop)
 
 
+# Testing functions
+print(binary_search(arr, -8, start, stop))
 print(binary_search(arr, 8, start, stop))
