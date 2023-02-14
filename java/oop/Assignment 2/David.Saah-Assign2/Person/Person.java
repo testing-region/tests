b424fc479b8816fb028465c38149165a53aef4aa
David Saah <dasorange.hope@gmail.com>
2023-02-14 11:55:50 +0000

    add constructors and gender array object

---
+++ java/oop/Assignment 2/David.Saah-Assign2/Person/Person.java
@@ -0,0 +1,140 @@
+
+// import needed packages
+import java.util.Random;
+import java.util.Arrays;
+
+/**
+ * This class defines a Person class with 3 attributes.
+ *
+ * @author David Saah
+ * @version 2.0
+ * @since 2023-02-06
+ *
+ */
+public class Person {
+
+    // Initialise Person attributes
+    private String name;
+    private byte age; // A person's age is rarely greater than 127 years.
+    private String gender;
+    private String[] genderTypes = { "male", "female" };
+
+    public Person() {
+        // create random object
+        Random random = new Random();
+
+        // randomly select an index in genderTypes
+        int option = random.nextInt(getGenderTypes().length);
+
+        setName("No name yet");
+        setAge((byte) 0);
+        setGender(getGenderTypes()[option]);
+    }
+
+    public Person(String name, byte age, String gender) {
+        setPerson(name, age, gender);
+    }
+
+    /**
+     * Gets the genderTypes array
+     *
+     * @return a list of gender types
+     *
+     */
+    public String[] getGenderTypes() {
+        return this.genderTypes;
+    }
+
+    /**
+     * Gets the name of the person
+     *
+     * @return the name of the person
+     *
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the age of the person
+     *
+     * @return the age of the person
+     *
+     */
+    public byte getAge() {
+        return age;
+    }
+
+    /**
+     * Gets the gender of the person
+     *
+     * @return the gender of the person
+     *
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * Sets the name of the person
+     *
+     * @param name the name of a person
+     *
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Sets the age of the person if the value chosen is not negative.
+     * Displays an error message if the age chosen is negative.
+     * Exits the program after displaying an error message.
+     *
+     * @param age the age chosen
+     *
+     */
+    public void setAge(byte age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            System.out.println("Error: The age entered is negative!");
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Sets the gender of the person.
+     * For simplicity, valid values for gender are "male" and "female".
+     * It checks if an invalid value for gender is given.
+     * Displays an error message if the chosen gender is invalid.
+     * Exits the program after displaying an error message.
+     *
+     * @param gender the gender of a person, whether "male" or "female"
+     *
+     */
+    public void setGender(String gender) {
+        boolean isValidGender = Arrays.stream(getGenderTypes())
+                .anyMatch(gender::equalsIgnoreCase);
+
+        if (isValidGender) {
+            this.gender = gender;
+        } else {
+            System.out.println("Error: The gender status is not found in our database!");
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Sets all the attributes of a person: name, age and gender.
+     *
+     * @param name   the name of the person
+     * @param age    the age of the person
+     * @param gender the gender of the person, whether "male" or "female"
+     *
+     */
+    public void setPerson(String name, byte age, String gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+}
