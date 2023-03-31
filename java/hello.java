6668b35724a944ce013c4b77a0391b946bdd55ab
David Saah <dasorange.hope@gmail.com>
2023-03-31 00:20:21 +0000

    new tests

--- java/hello.java
+++ java/hello.java
@@ -1,5 +1,6 @@
 class Hello {
-	public static void main(String[] args){
-		System.out.println("Hello World!");
+	public static void main(String[] args) {
+		String name = "Hello";
+		System.out.println(name);
 	}
 }
