7381e4d17760422d74438afe4406dcef645f6c8d
David Saah <dasorange.hope@gmail.com>
2023-04-14 12:21:11 +0000

    test constants

--- java/hello.java
+++ java/hello.java
@@ -1,6 +1,10 @@
 class Hello {
-	public static void main(String[] args) {
-		String name = "Hello";
-		System.out.println(name);
-	}
+
+    public static final double ESS_DISCOUNT = 0.2;
+
+    public static void main(String[] args) {
+        String name = "Hello";
+        System.out.println(name);
+        System.out.println(ESS_DISCOUNT);
+    }
 }
