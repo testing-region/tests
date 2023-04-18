6954745b020195dcd2fc4a3cfaa41de99c779e6b
David Saah <dasorange.hope@gmail.com>
2023-04-18 00:36:27 +0000

    add binary search test

--- DSA/binary-search/test1.py
+++ DSA/binary-search/test1.py
@@ -5,22 +5,22 @@ stop = len(arr) - 1
 
 
 def binary_search(arr, target, start, stop):
-    
-    # get the mid index of the start and stop indices in the 
+
+    # get the mid index of the start and stop indices in the
     # array to avoid slicing the array.
 
-    # no slicing prevents wrong indices from being returned if 
+    # no slicing prevents wrong indices from being returned if
     # the target number is present in the array.
     mid_index = (start+stop) // 2
-    
+
     if arr[mid_index] == target:
         return mid_index
-    
-    # if the end of the binary search is reached, the start and 
+
+    # if the end of the binary search is reached, the start and
     # stop values will be the same, hence stop the recursive call.
     if start == stop:
         return "Number not present in array"
-    
+
     if arr[mid_index] > target:
         return binary_search(arr, target, start, mid_index - 1)
     else:
