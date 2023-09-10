dbf050ccf809d4c6f54eee288cac6ca5d7e3a76c
David Saah <dasorange.hope@gmail.com>
2023-09-10 21:09:59 +0000

    update normalise url tests

--- golang/api/002/002_test.go
+++ golang/api/002/002_test.go
@@ -1,18 +1,16 @@
 package main_test
 
 import (
-	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/net/html"
 
 	. "api"
 )
 
 func TestNormaliseUrl(t *testing.T) {
 	url := "https://cht.SH/btrfs/"
-	actual := NormaliseUrl(url)
+	actual := NormaliseUrl("cht.sh", url)
 	expected := "cht.sh/btrfs"
 
 	t.Run("Normalise Url", func(t *testing.T) {
@@ -21,18 +19,8 @@ func TestNormaliseUrl(t *testing.T) {
 }
 
 func TestGetUrlsFromHtml(t *testing.T) {
-	r, err := http.Get("http://0.0.0.0:8080/")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	inputHtml, err := html.Parse(r.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	actual := GetUrlsFromHtml(inputHtml)
-	expected := []string{"cht.sh", "boot.dev"}
+	actual := GetUrlsFromHtml("0.0.0.0:8080")
+	expected := []string{"cht.sh", "0.0.0.0:8080/path/"}
 
 	t.Run("Check if urls were crawled successfully", func(t *testing.T) {
 		assert.Equal(t, expected, actual, "Link parsing failed")
