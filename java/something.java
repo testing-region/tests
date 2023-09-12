e7a99761ee0360ca55b995f1401b688b71329df5
David Saah <dasorange.hope@gmail.com>
2023-09-12 23:11:24 +0000

    add something.java

---
+++ java/something.java
@@ -0,0 +1,8 @@
+public class something {
+  public static void main(String[] args) {
+    System.out.printf("Name %s Unit Price %s Quantity\n", " ".repeat(12), " ".repeat(5));
+    for (int i = 0; i < 5; i++) {
+      System.out.printf("%s %s %.2f %s %d \n", "David Saah", " ".repeat(8), 2.33, " ".repeat(12), 5);
+    }
+  }
+}
