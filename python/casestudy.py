52cdb98a8a5ce5018e54d04d2bd133415fe34b11
David Saah <dasorange.hope@gmail.com>
2023-11-08 12:22:08 +0000

    document code
    - document code
    - save data to a file

--- python/casestudy.py
+++ python/casestudy.py
@@ -1,9 +1,30 @@
-class_list = {}
+# json will be used to load/save dictionary data to/from a file
+import json
+
+
+def load_data():
+    """
+    Loads previously saved data
+
+    Returns
+    -------
+    dict
+        a dictionary of students' data (having names and assignment data)
+    """
+    with open("./student_data.txt", "r") as f:
+        return json.load(f)  # reads a dictionary in a file
+
+
+class_list = load_data()
 
 
 def register():
+    """
+    Allows a user to register new students
+    """
     print("Enter an empty space when done!!!")
 
+    # enter students' name until the user enters an empty space
     while True:
         name = input("Enter student name: ")
         if name != "":
@@ -13,21 +34,34 @@ def register():
 
 
 def enter_grades():
+    """
+    Allows a user to enter the grades for students
+    for a particular assignment
+    """
     assignment_name = input("Enter the assignment name: ")
     total = int(input("Enter the total points: "))
 
+    # create a query for every student
     for key in class_list.keys():
         score = int(input(f"Enter the score for {key}: "))
 
+        # check if score exceeds total mark
+        # return error and prompt for another input
+        # P.S. Multiple input errors are not handled
         if score > total:
             print("Error: Exceeds total grade.")
             print(f"Try again: Score should be less than or equal to {total}")
             score = int(input(f"Enter the score for {key}: "))
 
+        # store the score for the particular assignment
         class_list[key][assignment_name] = score
 
 
 def list():
+    """
+    List the student names and their scores for each
+    corresponding assignment
+    """
     # key1 -> student name
     # val1 -> dictionary (assignment_name, grade)
     for key1, val1 in class_list.items():
@@ -42,23 +76,41 @@ def list():
         print()
 
 
+def save_data(data):
+    """
+    Saves students' data when the program exits
+
+    Parameters
+    -----------
+    data
+        a dictionary of students' data (having names and assignment data)
+    """
+    with open("./student_data.txt", "w") as f:
+        json.dump(data, f)  # writes a dictionary to a file
+
+
 # our menu
 while True:
+    print()
     print("1. Register students")
     print("2. Enter grades")
     print("3. List students and their grades")
     print("4. Exit")
+    print()
 
-    option = int(input("Select an option: "))
+    option = input("Select an option: ")
+    print()
 
-    if option == 1:
+    if option == "1":
         register()
-    elif option == 2:
+    elif option == "2":
         enter_grades()
-    elif option == 3:
+    elif option == "3":
         list()
-    elif option == 4:
+    elif option == "4":
+        save_data(class_list)
         break
     else:
         print("Invalid option, try again")
+        print()
         continue
